932cd77a7fa13ddbb93bacda91590b4c5c0721e9
Yashaswini K P <yashaswinikpyashu@gmail.com>
2025-09-16 22:45:50 +0530

    Add files via upload

---
+++ butterfly.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    //top
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<"*";
+        }
+        if(i!=n-1){
+            for(int j=0; j<(n*2)-2-(2*i); j++){
+                cout<<" ";
+            }
+        }
+        for(int j=0; j<i+1; j++){
+                cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    //bottom
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            cout<<"*";
+        }
+        if(i!=0){
+            for(int j=0; j<2*i; j++){
+                cout<<" ";
+            }
+        }
+        for(int j=i; j<n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    
+    return 0;
+}
